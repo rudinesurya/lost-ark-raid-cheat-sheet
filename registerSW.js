8c2b904188ead3b2fc87dd839adc14d5c8a2f437
Rudine Hartanto <rudine.hartanto@intellisense.io>
2025-01-13 23:33:45 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/lost-ark-raid-cheat-sheet/sw.js', { scope: '/lost-ark-raid-cheat-sheet/' })})}
